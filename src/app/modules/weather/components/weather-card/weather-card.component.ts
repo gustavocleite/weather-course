a106736624a312b43e04eb3a05d9bd36d8c73cb1
Gustavo C <gustavo@segredes.com.br>
2024-03-13 17:36:34 -0300

    add weather-card

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { WeatherDatas } from './../../../../models/interfaces/WeatherDatas';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FontAwesomeModule]
 })
-export class WeatherCardComponent implements OnInit{
+export class WeatherCardComponent {
 
     @Input() weatherDatasInput!: WeatherDatas;
-
-    ngOnInit(): void {
-      console.log('DADOS RECEBIDOS DO COMPONENTE PAI!:',
-      this.weatherDatasInput);
-    }
+    minTemperatureIcon = faTemperatureLow;
+    maxTemperatureIcon = faTemperatureHigh;
+    humidityIcon = faDroplet;
+    windIcon = faWind;
 }
